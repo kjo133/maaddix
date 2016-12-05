f0e7795c7e0f70fef4a06fc16ba2a7907ade9a9d
Kevin Olson <kevino93@vt.edu>
2016-12-05 17:15:13 -0500

    Dec 5 Changes

--- index.php
+++ index.php
@@ -4,21 +4,21 @@
 	<title>MAADDIX</title>
 
 
-		<link rel="stylesheet" type="text/css" href="css/jquery.fullPage.css" />
+	<link rel="stylesheet" type="text/css" href="css/jquery.fullPage.css" />
 	<link rel="stylesheet" type="text/css" href="css/examples.css">
-		<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/boostrap.min.css">
 
 
 
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>
-
+	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> 
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="js/jquery.fullPage.js"></script>
-
 	<script type="text/javascript">
 		$(document).ready(function() {
 			$('#fullpage').fullpage({
-								anchors: ['firstPage', 'secondPage', '3rdPage'],
+				anchors: ['firstPage', 'secondPage', '3rdPage'],
 				menu: '#menu',
 				verticalCentered: false,
 				css3:false
@@ -29,18 +29,52 @@
 
 </head>
 <body>
+
+  <nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+              <a class="navbar-brand" href="#"><img src="images/logo/Maaddix-03.jpg" style="height: 50px; width: 50px;" alt="Dispute Bills">
+      </a>
+      </div>
+      
+      <div id="navbar" class="navbar-collapse collapse">
+        <ul class="nav navbar-nav navbar-left">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Gallery</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li class="active"><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+      <!--/.nav-collapse -->
+    </div>
+    <!--/.container-fluid -->
+  </nav>
+
+
+
+
+<!-- 
 	<div id="header">
-		<ul id="menu">
+ 		<ul id="menu">
 			<li data-menuanchor="firstPage" class="active"><a href="#firstPage">Home</a></li>
 			<li data-menuanchor="secondPage"><a href="#secondPage">Gallery</a></li>
-			<li data-menuanchor="3rdPage"><a href="#3rdPage">About</a></li>
-			
-		</ul>
+			</ul>
+			<img src="images/logo/Maaddix-03.jpg" id="logo">
+			<ul id="menu">
+			<li data-menuanchor="3rdPage"><a href="#3rdPage">About!</a></li>
+			</ul>
 	</div>
+ -->
 
-
-
-	<div id="fullpage">
+ 	<div id="fullpage">
 		<div class="section active" id="section0">
 			<!-- <h1>MAADDIX</h1> 
 				<div id="home"><img src="images/logo/Maaddix-03.png" height="100px" width="100px"></div>
@@ -59,16 +93,24 @@
 					Name 3
 				</p>
 			</div>
-		   <!--  <div class="slide "><h1>Simple Demo</h1></div>
-		    <div class="slide "><h1>Only text</h1></div>
-		    <div class="slide"><h1>And text</h1></div>
-		    <div class="slide"><h1>And more text</h1></div> -->
 		</div>
-<!-- 		<div class="section" id="section2"><h1>No wraps, no extra markup</h1></div>
-		<div class="section" id="section3"><h1>Just the simplest demo ever</h1></div> -->
+	<div class="section" id="section2">
+			<h1>Gallery</h1>
+			<div class ="slide">
+				<h2>MAADDIX Mission</h2>
+				<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ul."</p>
+			</div>
+			
+		</div>
+
+
+		<!-- END ABOUT -->
 	</div>
 
 		<div id="footer">Footer</div>
+
+
+
 
 
 
