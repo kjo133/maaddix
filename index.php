b15f4dee7d1aa457cd280757250fcb88a933a10d
Kevin Olson <kolson@Administrators-MacBook-Pro-2.local>
2017-08-30 17:17:24 -0400

    Latest - back to bootstrap

--- index.php
+++ index.php
@@ -5,27 +5,71 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Maaddix</title>
-    <link rel="stylesheet" href="css/css/foundation.css">
-    <link rel="stylesheet" href="css/css/app.css">
-    <link rel="stylesheet" href="css/css/styles.css">
+    <!-- <link rel="stylesheet" href="css/css/foundation.css"> -->
+    <!-- <link rel="stylesheet" href="css/css/app.css"> -->
+    <!-- <link rel="stylesheet" href="css/css/styles.css"> -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+
   </head>
   <body>
     <!-- HEAD -->
-    <nav class="top-bar nav-desktop">
+<!--     <nav class="top-bar nav-desktop" style="border: solid red;">
       <div class="wrapper">
-        <div class="top-bar-left">
+        <div class="top-bar-left" style="border: solid blue;">
           <h3 class="site-logo">Maaddix</h3>
         </div>
        
-        <div class="top-bar-right">
+        <div class="top-bar-right" style="border: solid red;">
           <ul class="menu menu-desktop">
             <li><a href="#">Gallery</a></li>
-            <li><a href="#">About</a></li>
+            <li><a href="#about">About</a></li>
             <li><a href="#">Contact</a></li>
         </ul>
         </div>
       </div>
-   </nav>
+   </nav> -->
+
+   <nav class="navbar navbar-inverse navbar-static-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar3">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#"><img src="images/logo/maaddText.png" alt="">
+        </a>
+      </div>
+      <div id="navbar3" class="navbar-collapse collapse">
+        <ul class="nav navbar-nav navbar-right">
+          <li class="active"><a href="#">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#gallery">Gallery</a></li>
+
+          <!-- <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="#">Action</a></li>
+              <li><a href="#">Another action</a></li>
+              <li><a href="#">Something else here</a></li>
+              <li class="divider"></li>
+              <li class="dropdown-header">Nav header</li>
+              <li><a href="#">Separated link</a></li>
+              <li><a href="#">One more separated link</a></li>
+            </ul>
+          </li> -->
+        </ul>
+      </div>
+      <!--/.nav-collapse -->
+    </div>
+    <!--/.container-fluid -->
+  </nav>
+
+
+
+
 
    <!--   HERO SECTION -->
      <div class="hero">
@@ -37,17 +81,22 @@
          <a href="#" class="button">Learn More</a>
       </div> -->
         <div id="heroLogo">
-          <img src="images/logo/Maaddix-03.png">
+          <img src="images/logo/Maaddix-03.png" >
+          <h3>A Love Affair With Mountain Air</h3>
+          <h1 style="color:white; text-align: center;">A KEVIN OLSON WEBSITE PRODUCTION</h1>
         </div>
      	  
      </div>
 
      <!-- MAIN -->
-     <section class="main">
+     <section class="main" id="about">
        <div class="wrapper row">
          <div class="medium-12 column">
           <h1>Maaddix Mission</h1>
-           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+           <p>Mountain Air Addix (MAADDIX) is an apparel-logo lifestyle brand encompassing all mountain adventures and mountain sports.  We created this lifestyle brand for all mountain adventurers, for a culture that appreciates all the earth has to offer, and for those that are not ashamed of their love affair with mountain air.  Mountains are not meant to be barriers or hindrances, but meant to be overcome and conquered.  Your mountain is calling, go climb it and share it with MAADDIX.  
+           <br/>
+           Web Master: Kevin Olson
+           </p>
          </div>
          <!--  <div class="small-12 medium-6 column">
             <h1>Second Section</h1>
@@ -57,8 +106,40 @@
      </section>
 
      <section class="gallery">
-     	<div class="wrapper">
+
+     	<div class="wrapper" id="#gallery">
      		<h1>Adventure Gallery</h1>
+          <div class="ImageContainer">
+            <h2 class="text-center">Check out these awesome views and epic adventures taken by all of our mountain air addix.  <br/> Send us your latest and greatest conquests at mountainairaddix@gmail.com <br/> Also be sure to checkout Kevin Olson's Music!</h2>
+            <div class="lightbox-gallery">
+                <div><img src="images/images/1.JPG"></div>
+                <div><img src="images/images/2.JPG"></div>
+                <div><img src="images/images/3.JPG"></div>
+                <div><img src="images/images/4.JPG"></div>
+                <div><img src="images/images/5.JPG"></div>
+                <div><img src="images/images/6.JPG"></div>
+                 <div><img src="images/images/7.JPG"></div>
+                <div><img src="images/images/8.JPG"></div>
+                <div><img src="images/images/9.JPG"></div>
+                <div><img src="images/images/10.JPG"></div>
+                <div><img src="images/images/11.JPG"></div>
+                <div><img src="images/images/12.JPG"></div>
+                <div><img src="images/images/13.JPG"></div>
+                <div><img src="images/images/14.JPG"></div>
+                <div><img src="images/images/15.JPG"></div>
+                <div><img src="images/images/16.JPG"></div>
+                 <div><img src="images/images/17.JPG"></div>
+                <div><img src="images/images/18.JPG"></div>
+                <div><img src="images/images/19.JPG"></div>
+                <div><img src="images/images/20.JPG"></div>
+                <div><img src="images/images/21.JPG"></div>
+                <div><img src="images/images/22.JPG"></div>
+                <div><img src="images/images/23.JPG"></div>
+                <div><img src="images/images/24.JPG"></div>
+                <div><img src="images/images/25.JPG"></div>
+
+            </div>
+         </div>
      	</div>
      </section>
 
@@ -92,6 +173,7 @@
      </footer>
 
     <script src="js/vendor/jquery.js"></script>
+    <script src="js/lightbox.js"></script>
     <script src="js/vendor/what-input.js"></script>
     <script src="js/vendor/foundation.js"></script>
     <script src="js/app.js"></script>
